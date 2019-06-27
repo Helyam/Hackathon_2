2c5a6e9767a47bad7de6e01d88493dec35f81902
LudovicCocquyt <cocquyt.ludovic@laposte.net>
2019-06-27 02:25:09 +0200

    dynamisme home page =>ok

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,16 +4,21 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\TopicsRepository;
+use App\Repository\VoteRepository;
+
 
 class HomeController extends AbstractController
 {
     /**
      * @Route("/", name="home")
      */
-    public function index()
+    public function index(TopicsRepository $topicsRepository, VoteRepository $voteRepo)
     {
         return $this->render('home/accueil.html.twig', [
             'controller_name' => 'HomeController',
+            'topics' => $topicsRepository->findAll(),
+            'votes' => $voteRepo->findAll(),
         ]);
     }
 }
