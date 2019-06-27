d4c84e99ad9fa329bbe630d97ae2cf995e3b0cfc
Noel AN <noel.an@hotmail.fr>
2019-06-27 15:28:20 +0200

    view maire

--- src/Controller/MaireController.php
+++ src/Controller/MaireController.php
@@ -6,23 +6,25 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\TopicsRepository;
 use App\Repository\VoteRepository;
-use Symfony\Component\HttpFoundation\RedirectResponse;
 
-class HomeController extends AbstractController
+
+/**
+ * @Route("/maire")
+ */ 
+
+class MaireController extends AbstractController
 {
     /**
-     * @Route("/", name="home")
+     * @Route("/", name="maire_home")
      */
     public function index(TopicsRepository $topicsRepository, VoteRepository $voteRepo)
-    {   $user = $this->get('security.token_storage')->getToken()->getUser();
-
-        
-
+    {
         return $this->render('home/accueil.html.twig', [
             'controller_name' => 'HomeController',
-            'topics' => $topicsRepository->findAll(),
+            'topics' => $topicsRepository->findBy(['status' => 'Revu']),
             'votes' => $voteRepo->findAll(),
             'totalDecision' => $topicsRepository->findBy(['status' => ['Refusé', 'Réalisé']]),
         ]);
     }
+
 }
