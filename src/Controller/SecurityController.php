bab16736e50bfbf1f70a1439a4ff911d3ab504e2
helyam <helyam.zerrouki@gmail.com>
2019-06-28 10:18:17 +0200

    Navbar avec bouton connexion et deconnexion

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -9,6 +9,15 @@ use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
 {
+        /**
+     * @Route("/logout", name="app_logout", methods={"GET"})
+     */
+    public function logout()
+    {
+        // controller can be blank: it will never be executed!
+        throw new \Exception('Don\'t forget to activate logout in security.yaml');
+    }
+
     /**
      * @Route("/login", name="app_login")
      */
